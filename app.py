60f43f66fd7c06d17e79dc2ee938fb5144646e97
Maulidya Laura <mldyalauraa@gmail.com>
2026-06-30 09:46:04 +0700

    Update and rename app (1).py to app.py

--- app.py
+++ app.py
@@ -313,7 +313,7 @@ plt.rcParams.update({
 # ─────────────────────────────────────────────
 @st.cache_data
 def load_data():
-    return pd.read_csv("../data/label_spotify.csv")
+    return pd.read_csv("../label_spotify.csv")
 
 @st.cache_resource
 def load_model():
